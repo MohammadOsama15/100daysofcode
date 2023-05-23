4a9d7e911971dc1fb3ab99ff6731f9b9662e032a
Mohammad Osama <osama_Farhat@Outlook.com>
2023-05-23 13:34:41 -0400

    just updating saved files for day1

--- Day1/pracDay1.py
+++ Day1/pracDay1.py
@@ -7,3 +7,12 @@ print("Hello world!\nHello world!\nHello world!")
 
 #concatination:
 print ("Hello" + " " + "Angela")
+
+# String of characters, String concatenation making them one word. 
+#Synatax Error - forgot to add something that needs to be there, grammar is wrong like in english.
+#Indentation Error - Look at that where the error is, check if the indentation is proper.
+
+
+#Input will get the user input in console 
+# Then print() will print the word "Hello" and the user input
+print("Hello" + input("What is your name?"))
