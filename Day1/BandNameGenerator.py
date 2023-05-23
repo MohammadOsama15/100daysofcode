7475a471e06c47027f9388aa26c7a68fb6e6ae9e
Mohammad Osama <osama_Farhat@Outlook.com>
2023-05-23 13:32:21 -0400

    Day 1 completed with BandNameGenerator.py being the final project for the day.

---
+++ Day1/BandNameGenerator.py
@@ -0,0 +1,10 @@
+#1. Create a greeting for your program.
+print("Hello, welcome to the band name generator!\n")
+#2. Ask the user for the city that they grew up in.
+city = input("please input the city that you grew up in?\n")
+#3. Ask the user for the name of a pet.
+pet = input("please input your pets name?\n")
+#4. Combine the name of their city and pet and show them their band name.
+band_name = (city + " "+ pet)
+#5. Make sure the input cursor shows on a new line:
+print("Your band name can be "+ band_name +"\n")
---
+++ Day1/pracDay1.py
@@ -0,0 +1,9 @@
+#Day1 5/18/2023
+#Printing, Commenting, Debugging, String Manipulation and Variables
+print("Hello world!\nHello world!")
+
+#printing on multiple lines
+print("Hello world!\nHello world!\nHello world!")
+
+#concatination:
+print ("Hello" + " " + "Angela")
